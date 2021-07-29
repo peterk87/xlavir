0f8320f8960db7037f83b65a6f434899bdba2e31
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2021-07-29 12:44:24 -0500

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/peterk87/xlavir',
-    version='0.4.2',
+    version='0.4.3',
     zip_safe=False,
 )
